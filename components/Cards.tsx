004a51489330dab5f43f5fe9314fd723ede3e682
AqibJavaid899 <aqibjaved0910@gmail.com>
2023-01-19 23:14:49 +0500

    fix: add the key prop to the card component to avoid the key prop error in deployment

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -7,8 +7,8 @@ type Props = {};
 const Cards = (props: Props) => {
   return (
     <main className="mt-5 flex flex-col max-w-[44rem] mx-auto px-4">
-      {data.links.map((link: Link) => (
-        <Card link={link} />
+      {data.links.map((link: Link, key: number) => (
+        <Card key={key} link={link} />
       ))}
     </main>
   );
